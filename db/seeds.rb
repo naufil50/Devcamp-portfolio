00896dffcf5ca38a96ce9d0bf2a9e07ac1d5f2da
naufil kharbe <naufil50@gmail.com>
2019-08-30 02:09:56 +0530

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the"
+	)
+end	
+
+puts "10 times Blog created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+		)
+end
+
+
+puts "5 times Skill created"
+
+
+9.times do |portfolio|
+	Portfolio.create!(
+	 title: "Portfolio title #{portfolio}",
+     subtitle: "My great service",
+     body: "Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a model–view–controller (MVC) ",
+     main_image:  "https://via.placeholder.com/300x200",
+     thumb_image: "https://via.placeholder.com/300x150"
+		)
+end
+
+puts "9 times portfolio created"
+
