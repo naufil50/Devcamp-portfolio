420fc107fab96a16d3a13fab1baef8e2ed13b685
naufil kharbe <naufil50@gmail.com>
2019-08-30 23:00:57 +0530

    Edit and updated functionality is done

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,4 +19,22 @@ class PortfoliosController < ApplicationController
         end
 
 	end
+
+
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+		
+	def update
+	    @portfolio_item = Portfolio.find(params[:id])
+
+	    respond_to do |format|
+	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	        format.html { redirect_to portfolios_path, notice: 'Portfolio item was updated successfully.' }
+	      else
+	        format.html { render :new }
+          end
+        end
+
+	end
 end
