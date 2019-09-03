2459b337a220a0d4295fabf3cbf59e14122bca0d
naufil kharbe <naufil50@gmail.com>
2019-09-03 10:11:43 +0530

    Strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
 	end
 		
 	def create
-	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+	    @portfolio_item = Portfolio.new(portfolio_params)
 
 	    respond_to do |format|
 	      if @portfolio_item.save
@@ -35,7 +35,7 @@ class PortfoliosController < ApplicationController
 	    @portfolio_item = Portfolio.find(params[:id])
 
 	    respond_to do |format|
-	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	      if @portfolio_item.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: 'Portfolio item was updated successfully.' }
 	      else
 	        format.html { render :new }
@@ -55,6 +55,18 @@ class PortfoliosController < ApplicationController
              format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
              format.json { head :no_content }
            end
+	end
+
+	private
+
+	def portfolio_params
+
+		params.require(:portfolio).permit(:title,
+										  :subtitle,
+										  :body, 
+										  technologies_attributes: [:name])
+											)
+
 	end
 		
 	
