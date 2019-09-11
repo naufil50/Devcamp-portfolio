ed195091d11fd79bc03b0604eee265bbafde0944
naufil kharbe <naufil50@gmail.com>
2019-09-12 01:48:50 +0530

    Initial javascript changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-	// This file is automatically compiled by Webpack, along with any other files
+// This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -6,6 +6,11 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery-ui
+//= require html.sortable
+//= require turbolinks
+//= require_tree .
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
